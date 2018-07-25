d96db4e0efb9f3fe88a7acdb00c339d7a2da6f1e
50kudos <50th.zaint@gmail.com>
2018-07-26 02:39:50 +0700

    Add accessible errors attribute for post and patch

--- lib/thera/base.rb
+++ lib/thera/base.rb
@@ -6,7 +6,10 @@ module Thera
     base_uri 'https://app.thera-link.com/api/v2'
     headers 'Accept': 'application/json', 'Content-Type': 'application/json'
 
+    attr_reader :errors
+
     def initialize(token)
+      @errors = {}
       self.class.headers Authorization: "Bearer #{token}"
     end
 
@@ -17,11 +20,15 @@ module Thera
       end
 
       def post(path, attrs={})
-        self.class.post(path, attrs)
+        response = self.class.post(path, attrs)
+        @errors = response.fetch('errors', {})
+        response
       end
 
       def patch(path, attrs={})
-        self.class.patch(path, attrs)
+        response = self.class.patch(path, attrs)
+        @errors = response.fetch('errors', {})
+        response
       end
 
   end
